44386f46dc5382e7a1b5692f6b06d618b737fd90
Lol3rrr <lol3r@lol3r.net>
2021-11-19 10:23:42 +0100

    Updated the Error messages some more again

--- compiler/src/main.rs
+++ compiler/src/main.rs
@@ -28,25 +28,20 @@ fn main() {
                         dbg!("EOF");
                     }
                     SyntaxError::UnexpectedToken { got, expected } => {
-                        let (line_start_offset, lines) = got
-                            .source()
-                            .get_lines_of_chars(got.source_area().clone())
-                            .unwrap();
-
-                        let label_offsets = got.source_area().start - line_start_offset;
+                        let content = got.source().content();
+                        let content_area = got.source_area();
 
                         Report::build(ReportKind::Error, (), 0)
                             .with_message("Unexpected Token")
                             .with_label(
-                                Label::new(label_offsets..got.content().len() + label_offsets)
-                                    .with_message("This was given"),
+                                Label::new(content_area.clone()).with_message("This was given"),
                             )
                             .with_label(
-                                Label::new(label_offsets..got.content().len() + label_offsets)
+                                Label::new(content_area.clone())
                                     .with_message(format!("Expected {:?}", expected)),
                             )
                             .finish()
-                            .print(Source::from(lines))
+                            .print(Source::from(content))
                             .unwrap();
                     }
                 }
